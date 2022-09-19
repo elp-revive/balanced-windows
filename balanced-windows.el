59764dc43e5115e3939c6f24c1865c440728f5da
Jen-Chieh Shen <jcs090218@gmail.com>
2022-09-19 16:23:40 +0800

    port to command

--- balanced-windows.el
+++ balanced-windows.el
@@ -1,9 +1,10 @@
 ;;; balanced-windows.el --- Keep windows balanced -*- lexical-binding: t; -*-
 
 ;; Author: wouter bolsterlee <wouter@bolsterl.ee>
+;; Maintiainer: Jen-Chieh Shen <jcs090218@gmail.com>
 ;; Keywords: convenience
-;; URL: https://github.com/wbolster/emacs-balanced-windows
-;; Package-Requires: ((emacs "25"))
+;; URL: https://github.com/elp-revive/emacs-balanced-windows
+;; Package-Requires: ((emacs "25.1"))
 ;; Version: 1.0.0
 
 ;; Copyright 2019 wouter bolsterlee. Licensed under the 3-Clause BSD License.
@@ -21,21 +22,29 @@
   :group 'windows
   :prefix "balanced-windows-")
 
-(defcustom balanced-windows-functions
+(defcustom balanced-windows-commands
   '(delete-window quit-window split-window)
   "Functions needing advice to keep windows balanced."
-  :group 'balanced-windows
-  :type '(repeat function))
+  :type '(repeat function)
+  :group 'balanced-windows)
+
+(defcustom balanced-windows-interactive-only t
+  "Balance windows only when called interactively."
+  :type 'boolean
+  :group 'balanced-windows)
 
 (defun balanced-windows--advice (&rest _ignored)
   "Balance windows (intended as :after advice); any args are ignored."
-  (balance-windows))
+  (when (or (null balanced-windows-interactive-only)
+            (and (called-interactively-p 'interactive)
+                 (memq this-command balanced-windows-commands)))
+    (balance-windows)))
 
 ;;;###autoload
 (define-minor-mode balanced-windows-mode
   "Global minor mode to keep windows balanced at all times."
   :global t
-  (dolist (fn balanced-windows-functions)
+  (dolist (fn balanced-windows-commands)
     (if balanced-windows-mode
         (advice-add fn :after #'balanced-windows--advice)
       (advice-remove fn #'balanced-windows--advice)))
